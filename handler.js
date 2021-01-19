49fdfa506d150d7d32744e2f761bb4586c94c74d
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2021-01-19 22:18:46 +1100

    Add dynamodb tables

--- handler.js
+++ handler.js
@@ -1,25 +1,82 @@
-"use strict";
-
-var S3 = require("aws-sdk");
+const { S3, DynamoDB } = require("aws-sdk");
 
 module.exports.requestUploadURL = (event, context, callback) => {
-  var s3 = new S3();
-  var params = JSON.parse(event.body);
+  const s3 = new S3();
+  const docClient = new DynamoDB.DocumentClient();
 
-  var s3Params = {
-    Bucket: "slsupload",
+  const params = JSON.parse(event.body);
+
+  const s3Params = {
+    Bucket: "spritey-upload",
     Key: params.name,
     ContentType: params.type,
     ACL: "public-read",
   };
 
-  var uploadURL = s3.getSignedUrl("putObject", s3Params);
+  const uploadURL = s3.getSignedUrl("putObject", s3Params);
+
+  const item = {
+    s3Path: uploadURL,
+    tag: params.tag,
+  };
+
+  docClient.put({
+    TableName: "sprites",
+    Item: item,
+  });
+
+  docClient.put({
+    TableName: "tags",
+    Item: {
+      tag,
+    },
+  });
 
   callback(null, {
     statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin": "https://spritey.tducasse.com",
     },
-    body: JSON.stringify({ uploadURL: uploadURL }),
+    body: JSON.stringify({ uploadURL }),
+  });
+};
+
+module.exports.getTags = async (event, context, callback) => {
+  const docClient = new DynamoDB.DocumentClient();
+
+  const queryParams = {
+    TableName: "tags",
+  };
+
+  const data = await docClient.scan(queryParams).promise();
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "https://spritey.tducasse.com",
+    },
+    body: JSON.stringify({ data }),
+  });
+};
+
+module.exports.getSprites = async (event, context, callback) => {
+  const docClient = new DynamoDB.DocumentClient();
+
+  const params = JSON.parse(event.body);
+
+  const queryParams = {
+    TableName: "sprites",
+    ExpressionAttributeNames: { "#tag": "tag" },
+    KeyConditionExpression: "#tag = :tag",
+    ExpressionAttributeValues: { ":tag": params.tag },
+  };
+
+  const data = await docClient.query(queryParams).promise();
+
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "https://spritey.tducasse.com",
+    },
+    body: JSON.stringify({ data }),
   });
 };
